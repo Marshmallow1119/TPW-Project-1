7c85f9a55a2d9a33a51dd9ea39a6e31cd54cd682
alof04 <afonso.ferreira@ua.pt>
2024-10-23 23:48:08 +0100

    modelos começados

--- merchify/app/models.py
+++ merchify/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    profile_picture = models.ImageField(upload_to='profile_pictures')
+    numberOfPurchases = models.IntegerField(default=0)
+
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to='products/')
+    artist = models.CharField(max_length=50)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='products')
+
