77805eb0938c646aec713aa248209002009b6da8
Marshmallow1119 <tomasbras@ua.pt>
2024-10-24 20:42:24 +0100

    criar templates e começar (página principal) home

--- merchify/app/views.py
+++ merchify/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def artistas(request):
+    return render(request, 'artistas.html')
+
